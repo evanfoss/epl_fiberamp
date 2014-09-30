bf45d785a1ccec4afe36596389f440ed211deca7
Evan Foss <evanfoss@gmail.com>
2014-09-30 17:14:39 -0500

    backlog

--- opticamp.cmd
+++ opticamp.cmd
@@ -23,6 +23,8 @@ ChangePinName(CONN3, 8, 8)
 ChangePinName(CONN3, 6, 6)
 ChangePinName(CONN3, 4, 4)
 ChangePinName(CONN3, 2, 2)
+ChangePinName(CONN3, 1, 1)
+ChangePinName(CONN3, 2, 2)
 
 # Start of element CONN2
 ChangePinName(CONN2, 11, 11)
@@ -37,28 +39,62 @@ ChangePinName(CONN2, 8, 8)
 ChangePinName(CONN2, 6, 6)
 ChangePinName(CONN2, 4, 4)
 ChangePinName(CONN2, 2, 2)
+ChangePinName(CONN2, 1, 1)
+ChangePinName(CONN2, 2, 2)
 
-# Start of element CONN1
-ChangePinName(CONN1, 5, 5)
-ChangePinName(CONN1, 3, 3)
-ChangePinName(CONN1, 1, 1)
-ChangePinName(CONN1, 6, 6)
-ChangePinName(CONN1, 4, 4)
-ChangePinName(CONN1, 2, 2)
+# Start of element C10
+ChangePinName(C10, 2, 2)
+ChangePinName(C10, 1, 1)
 
-# Start of element C5
-ChangePinName(C5, 2, 2)
-ChangePinName(C5, 1, 1)
-ChangePinName(C5, 2, 2)
-ChangePinName(C5, 1, 1)
-ChangePinName(C5, 2, 2)
-ChangePinName(C5, 1, 1)
+# Start of element C9
+ChangePinName(C9, 2, 2)
+ChangePinName(C9, 1, 1)
+
+# Start of element C8
+ChangePinName(C8, 2, 2)
+ChangePinName(C8, 1, 1)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 2, 2)
+
+# Start of element C7
+ChangePinName(C7, 2, 2)
+ChangePinName(C7, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element U3
+ChangePinName(U3, 3, IN+)
+ChangePinName(U3, 5, NULL)
+ChangePinName(U3, 7, +V)
+ChangePinName(U3, 1, NULL)
+ChangePinName(U3, 8, N.C.)
+ChangePinName(U3, 6, OUT)
+ChangePinName(U3, 4, V-)
+ChangePinName(U3, 2, IN-)
+
+# Start of element R8
+ChangePinName(R8, 1, 1)
+ChangePinName(R8, 2, 2)
+
+# Start of element C6
+ChangePinName(C6, 2, 2)
+ChangePinName(C6, 1, 1)
 
 # Start of element R3
 ChangePinName(R3, 1, 1)
 ChangePinName(R3, 2, 2)
-ChangePinName(R3, 1, 1)
-ChangePinName(R3, 2, 2)
+
+# Start of element C5
+ChangePinName(C5, 2, 2)
+ChangePinName(C5, 1, 1)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
 
 # Start of element U2
 ChangePinName(U2, 3, IN+)
@@ -69,40 +105,18 @@ ChangePinName(U2, 8, N.C.)
 ChangePinName(U2, 6, OUT)
 ChangePinName(U2, 4, V-)
 ChangePinName(U2, 2, IN-)
-ChangePinName(U2, 3, IN+)
-ChangePinName(U2, 5, NULL)
-ChangePinName(U2, 7, +V)
-ChangePinName(U2, 1, NULL)
-ChangePinName(U2, 8, N.C.)
-ChangePinName(U2, 6, OUT)
-ChangePinName(U2, 4, V-)
-ChangePinName(U2, 2, IN-)
 
-# Start of element R4
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
-ChangePinName(R4, 1, 1)
-ChangePinName(R4, 2, 2)
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
 
 # Start of element C3
-ChangePinName(C3, 2, 2)
-ChangePinName(C3, 1, 1)
 ChangePinName(C3, 2, 2)
 ChangePinName(C3, 1, 1)
 
 # Start of element C4
 ChangePinName(C4, 2, 2)
 ChangePinName(C4, 1, 1)
-
-# Start of element R2
-ChangePinName(R2, 1, 1)
-ChangePinName(R2, 2, 2)
-ChangePinName(R2, 1, 1)
-ChangePinName(R2, 2, 2)
-ChangePinName(R2, 1, 1)
-ChangePinName(R2, 2, 2)
 
 # Start of element U1
 ChangePinName(U1, 5, Trim+)
@@ -122,6 +136,7 @@ ChangePinName(C1, 2, 2)
 ChangePinName(C1, 1, 1)
 
 # Start of element D1
+ChangePinName(D1, 3, 3)
 ChangePinName(D1, 2, 2)
 ChangePinName(D1, 1, 1)
 
